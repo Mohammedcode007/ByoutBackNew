8fc9093b08d449b5260bdab4ead9cecf0d3a4ccb
Mohammedcode007 <code.hassan.1992@gmail.com>
2025-12-07 11:20:54 +0200

    first

--- firebase.js
+++ firebase.js
@@ -1,9 +1,18 @@
 // firebase.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/firebaseServiceAccount.json'); // المسار حسب مكان الملف
+require('dotenv').config(); // تحميل متغيرات البيئة
 
+// إنشاء كائن التهيئة من env
+const serviceAccount = {
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
+
+// تهيئة Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 module.exports = admin;
