d8e1ffcc1825bcccd841c3daff5eaf4925b73c20
Mohammedcode007 <code.hassan.1992@gmail.com>
2025-12-07 12:39:23 +0200

    first

--- config/db.js
+++ config/db.js
@@ -1,30 +1,13 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI); // Options مش محتاجين
-//     console.log('MongoDB Connected');
-//   } catch (err) {
-//     console.error('Failed to connect to MongoDB:', err.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-import mongoose from "mongoose";
-
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
-async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => mongoose);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI); // Options مش محتاجين
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err.message);
+    process.exit(1);
   }
+};
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
-
-export default connectDB;
+module.exports = connectDB;
